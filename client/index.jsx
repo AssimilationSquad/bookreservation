ce2efabf63500287d5f5645f6ec54a00d9abb3da
Kent Liu <kentliu@gmail.com>
2018-11-05 15:09:30 -0800

    made changes to component to provide a global name of Book

--- client/index.jsx
+++ client/index.jsx
@@ -1,7 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './app.jsx';
-// import Calendar from './calendar.jsx';
-// import Guests from './guests.jsx';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+window.Book = App;
