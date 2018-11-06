b715034524ebfbf66970b635df72dbd34ff4b18d
Kent Liu <kentliu@gmail.com>
2018-11-05 16:05:54 -0800

    added api domain and cors

--- server/server.js
+++ server/server.js
@@ -11,6 +11,12 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.static(path.join(__dirname, '../', 'public')));
 app.use('/rooms', express.static(path.join(__dirname, '../', 'public')));
 app.get('/rooms/:roomId', (req, res) => {
